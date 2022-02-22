12879e22ceab56c1b533e2b7ae263036bb13f38d
Lontchi Frank <fodjof6@gmail.com>
2022-02-22 22:36:06 +0100

    Account Detail

--- src/pages/FirstVr.jsx
+++ src/pages/FirstVr.jsx
@@ -7,7 +7,7 @@ export default class FirstVr extends React.Component {
   render() {
     return (
       <div>
-        <div className="case">
+        {/* <div className="case">
           <nav className="navs"></nav>
           <div className="case1">
             <div className="case-txt">
@@ -15,21 +15,7 @@ export default class FirstVr extends React.Component {
                 Create Account For Bank Owners Digitally, To be recognise
                 everywhere by any Bank.
               </h1>
-              {/* <div className="case-btn">
-                <button className="btn">
-                  <Link to="/form"> Start Create</Link>
-                </button>
-              </div> */}
-              {/* <div className="case-img">
-                <img
-                  src="/3.png"
-                  alt=""
-                  style={{
-                    width: "45%",
-                    height: "20%",
-                  }}
-                />
-              </div> */}
+            
             </div>
             <div className="case-btn">
               <button className="btn">
@@ -44,11 +30,48 @@ export default class FirstVr extends React.Component {
               style={{
                 width: "90%",
                 height: "70%",
-                // filter: "drop-shadow(30px 10px 4px #ccc)",
               }}
             />
           </div>
-        </div>
+        </div> */}
+        <section id="hero" class="hero d-flex align-items-center">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-6 d-flex flex-column justify-content-center">
+                <h1 data-aos="fade-up">
+                  We offer modern solutions for account Details to your business
+                </h1>
+                <h2 data-aos="fade-up" data-aos-delay="400">
+                  We are create platform for your user data to be stored, and
+                  saved. So as for your data to be editale at anytime.
+                </h2>
+                <div data-aos="fade-up" data-aos-delay="600">
+                  <div class="text-center text-lg-start">
+                    <a
+                      style={{
+                        textDecoration: "none",
+                        listStyle: "none",
+                        padding: "1.6em",
+                      }}
+                      href="/form"
+                      className="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center"
+                    >
+                      <span>Get Started</span>
+                      <i class="bi bi-arrow-right"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="col-lg-6 hero-img"
+                data-aos="zoom-out"
+                data-aos-delay="200"
+              >
+                <img src="/4.png" class="img-fluid" alt="" />
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     );
   }
